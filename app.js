944bbd85a23a359862c0b38e2e9e84481e22d3c0
jeanP-tech <jeanP.tech@gmail.com>
2019-12-24 23:07:48 -0500

    Add css

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+const canvas = document.getElementById("jsCanvas");
+
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+    console.log(event);
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+}
